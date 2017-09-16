b5454eb9e0c09b584a875ee7727779d935a5abf8
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-09-16 19:51:49 +0300

    add bootstrap views

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'devise', '~> 4.1.1'
+gem 'devise-i18n'
+gem 'jquery-rails'
+gem 'rails', '~> 4.2.6'
+gem 'rails-i18n'
+gem 'twitter-bootstrap-rails'
+gem 'uglifier', '>= 1.3.0'
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'byebug'
+  gem 'sqlite3'
+end
---
+++ app/models/event.rb
@@ -0,0 +1,21 @@
+# (с) goodprogrammer.ru
+#
+# Модель события
+class Event < ActiveRecord::Base
+  belongs_to :user
+
+  # У события много комментариев и подписок
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+
+  # У события много подписчиков (объекты User), через таблицу subscriptions,
+  # по ключу user_id
+  has_many :subscribers, through: :subscriptions, source: :user
+
+  validates :user, presence: true
+
+  validates :title, presence: true, length: {maximum: 255}
+
+  validates :address, presence: true
+  validates :datetime, presence: true
+end
